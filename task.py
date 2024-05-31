3bce09b8538ac741a24464e8d0f7695818abafd4
lum1n0va <djh5658@yahoo.com>
2024-05-31 13:21:12 -0400

    Add files via upload

--- task.py
+++ task.py
@@ -1,2 +1,130 @@
-def my_func():
-    return "Hello World"
+def conv_num(num_str):
+    """
+    Takes a string in the form of decimal or hexadecimal
+    Converts the strings to a base 10 number and returns it
+    """
+    sign_flag = False
+    first_char_zero_flag = False
+    second_char_x_flag = False
+    decimal_flag = False
+
+    "Check to see if nothing was entered"
+    if len(num_str) == 0:
+        return None
+
+    "Validate input prior to conversion"
+    for char in range(0, len(num_str)):
+        converted_char = ord(num_str[char])
+        if char == 0 and converted_char == 45:  # Checks if the first char is a minus sign
+            sign_flag = True
+        elif converted_char == 46:  # Checks if the char is a decimal point
+            if second_char_x_flag is True:
+                return None
+            elif decimal_flag is False:
+                decimal_flag = True
+            else:
+                return None
+        elif 48 <= converted_char <= 57:  # Checks if the char is a number
+            if sign_flag is True and char == 1 and converted_char == 48:
+                first_char_zero_flag = True
+            elif char == 0 and converted_char == 48:
+                first_char_zero_flag = True
+        elif 65 <= converted_char <= 70:  # Checks if the char is an uppercase letter
+            if first_char_zero_flag is False or second_char_x_flag is False:
+                return None
+        elif 97 <= converted_char <= 102:  # Checks if the char is a lowercase letter
+            if first_char_zero_flag is False or second_char_x_flag is False:
+                return None
+        elif converted_char == 88 or converted_char == 120:  # Checks if the char is an upper or lowercase x
+            if decimal_flag is True:
+                return None
+            elif sign_flag is True and char == 2:
+                second_char_x_flag = True
+            elif char == 1:
+                second_char_x_flag = True
+            else:
+                return None
+        else:
+            return None
+
+    "Convert to the appropriate format"
+    if first_char_zero_flag is True and second_char_x_flag is True:
+        converted_num = convert_hex(num_str)
+    else:
+        converted_num = convert_decimal(num_str)
+
+    return converted_num
+
+
+def convert_hex(num_str):
+    """
+        Helper function use to convert a hexadecimal number to a base 10 number
+        Returns the converted number
+    """
+    num_places = len(num_str) - 1
+    sign_flag = False
+    converted_hex = 0
+
+    for char in range(0, len(num_str)):
+        converted_char = ord(num_str[char])
+        if converted_char == 48:  # Checks if the char is a zero
+            num_places -= 1
+        elif converted_char == 88 or converted_char == 120:  # Checks if the char is an upper or lowercase x
+            num_places -= 1
+        elif 49 <= converted_char <= 57:  # Checks if the char is a number
+            converted_char -= 48
+            converted_char *= pow(16, num_places)
+            num_places -= 1
+            converted_hex += converted_char
+        elif 65 <= converted_char <= 70:  # Checks if the char is an uppercase letter
+            converted_char -= 55
+            converted_char *= pow(16, num_places)
+            num_places -= 1
+            converted_hex += converted_char
+        elif 97 <= converted_char <= 102:  # Checks if the char is a lowercase letter
+            converted_char -= 87
+            converted_char *= pow(16, num_places)
+            num_places -= 1
+            converted_hex += converted_char
+        else:
+            sign_flag = True
+            num_places -= 1
+
+    if sign_flag is True:
+        converted_hex *= -1
+
+    return converted_hex
+
+
+def convert_decimal(num_str):
+    """
+        Helper function use to convert a decimal number to a base 10 number
+        Returns the converted number
+    """
+    num_places = len(num_str) - 1
+    sign_flag = False
+    converted_decimal = 0
+    decimal_flag = False
+
+    for char in range(0, len(num_str)):
+        converted_char = ord(num_str[char])
+        if 48 <= converted_char <= 57:  # Checks if the char is a number
+            converted_char -= 48
+            converted_char *= pow(10, num_places)
+            converted_decimal += converted_char
+            if decimal_flag is True:
+                converted_decimal = round(converted_decimal,
+                                          num_places * -1)  # Corrects rounding errors created by pow()
+            num_places -= 1
+        elif converted_char == 46:  # Checks if the char is a decimal point
+            decimal_flag = True
+            converted_decimal /= pow(10, num_places + 1)
+            num_places = -1  # Corrects num_places for use with numbers after the decimal point
+        else:
+            sign_flag = True
+            num_places -= 1
+
+    if sign_flag is True:
+        converted_decimal *= -1
+
+    return converted_decimal
--- tests.py
+++ tests.py
@@ -1,16 +1,76 @@
 import unittest
-import task
+from task import conv_num
 
 
 class TestCase(unittest.TestCase):
 
     def test1(self):
-        expected = 'Hello World'
-        self.assertEqual(task.my_func(), expected)
+        self.assertTrue(True)
 
-    def test2(self):
-        expected = "Hola World"
-        self.assertEqual(task.my_func(), expected)
+    def test_no_input(self):
+        """Test checks to see if no input is accepted"""
+
+        self.assertEqual(conv_num(''), None)
+
+    def test_only_space(self):
+        """Test checks to see if a space is accepted"""
+
+        self.assertEqual(conv_num(' '), None)
+
+    def test_space_in_front(self):
+        """Test checks to see a space in front is accepted"""
+
+        self.assertEqual(conv_num(' 12345'), None)
+
+    def test_space_in_middle(self):
+        """Test checks to see if a space in the middle is accepted"""
+
+        self.assertEqual(conv_num('0x AD4'), None)
+
+    def test_space_at_back(self):
+        """Test checks to see if a space at the back is accepted"""
+
+        self.assertEqual(conv_num('12345 '), None)
+
+    def test_multiple_decimals(self):
+        """Test checks to see if multiple decimal points are accepted"""
+
+        self.assertEqual(conv_num('12.3.45'), None)
+
+    def test_hex_and_decimal_point(self):
+        """Test checks to see if mixed format with hex and decimal points are accepted"""
+
+        self.assertEqual(conv_num('0xFF.02'), None)
+
+    def test_mulitple_negative_signs(self):
+        """Test checks to see if multiple negative signs are accepted"""
+
+        self.assertEqual(conv_num('-0xAD4-'), None)
+
+    def test_no_leading_zero(self):
+        """Test checks to see if a decimal without a zero accepted"""
+
+        self.assertEqual(conv_num('.45'), 0.45)
+
+    def test_no_trailing_zero(self):
+        """Test checks to see if decimals without a trailing zero are returned properly"""
+
+        self.assertEqual(conv_num('123.'), 123.0)
+
+    def test_decimal_with_letter(self):
+        """Test checks to see if decimals with letters are accepted"""
+
+        self.assertEqual(conv_num('12345A'), None)
+
+    def test_hex_char_out_of_range(self):
+        """Test checks to see if non hex characters are accepted"""
+
+        self.assertEqual(conv_num('0xAg4'), None)
+
+    def test_hex_without_zero(self):
+        """Test checks to see if hex entries without the zero are accepted"""
+
+        self.assertEqual(conv_num('X124'), None)
 
 
 if __name__ == '__main__':
